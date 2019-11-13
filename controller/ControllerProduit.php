6af260071c056c85d5819f03e54ebd6b8b50d868
Simon Moulin <simon.moulin@etu.umontpellier.fr>
2019-11-13 09:40:03 +0100

    Image par default si pas d'image upload

--- controller/ControllerProduit.php
+++ controller/ControllerProduit.php
@@ -31,14 +31,16 @@ class ControllerProduit
         }
     }
 
-    public static function showError(){
+    public static function showError()
+    {
         $controller = "";
         $view = 'error';
         $pagetitle = 'Erreur !';
         require(File::build_path(array("view", "view.php")));
     }
 
-    public static function delete(){
+    public static function delete()
+    {
         $id = $_GET['id'];
         $v = ModelProduit::deleteById($id);
         $controller = 'produits';
@@ -50,7 +52,8 @@ class ControllerProduit
         }
     }
 
-    public static function create(){
+    public static function create()
+    {
         $controller = "produits";
         $view = 'create';
         $pagetitle = "Creation d'un produit";
@@ -58,34 +61,31 @@ class ControllerProduit
     }
 
     public static function created(){
-    {
+        {
+            $name = $_FILES['fileToUpload']['name'];
+            $pic_path = "images/$name";
 
-        
-        $name = $_FILES['fileToUpload']['name'];
-        $pic_path = "images/$name";
+            if (is_uploaded_file($_FILES['fileToUpload']['tmp_name'])) {
+                $controller = "produits";
+            }
+            if (!move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $pic_path)) {
+                $name = "default_image.jpg";
+            }
 
-        if (is_uploaded_file($_FILES['fileToUpload']['tmp_name'])) {
-            echo "ok";
-         }
-        if (!move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $pic_path)) {
-            echo "erreur upload";
-            //$name="default_image.jpg";
-        }
-        
-
-        $prod = new ModelProduit(0,$_POST['name'], $_POST['desc'], $_POST['price'], $name);
-
-        $saveEx = $prod->save();
-
-        if ($saveEx == false) {
-            $controller = "";
-            $view = 'error';
-            $pagetitle = 'Erreur !';
+            $view = 'list';
+            $pagetitle = 'Produit ajouté avec succès !';
             require(File::build_path(array("view", "view.php")));
+
+            $prod = new ModelProduit(0, $_POST['name'], $_POST['desc'], $_POST['price'], $name);
+
+            $saveEx = $prod->save();
+
+            if ($saveEx == false) {
+                $controller = "";
+                $view = 'error';
+                $pagetitle = 'Erreur !';
+                require(File::build_path(array("view", "view.php")));
+            }
         }
     }
-
-    }
-
 }
-?>
--- view/produits/list.php
+++ view/produits/list.php
@@ -1,24 +1,27 @@
 <div class="row">
-<?php
+    <?php
     foreach ($tab_prod as $p) {
-        $url_product = "index.php?action=read&id=".$p->getId();
+        $url_product = "index.php?action=read&id=" . $p->getId();
+        $url_delete = "index.php?action=delete&id=" . $p->getId();
         echo '
     <div class="col s9 m3">
     <div class="card">
             <div class="card-image">
                 <img src="./images/' . $p->getImageUrl() . '">
-                <a class="btn-floating halfway-fab waves-effect waves-light red" href="'.$url_product.'"><i class="material-icons">add_shopping_cart</i></a>
+                <a class="btn-floating halfway-fab waves-effect waves-light red" href="' . $url_product . '"><i class="material-icons">add_shopping_cart</i></a>
             </div>
             
             <div class="card-content">
                 <span class="card-title">' . $p->getName() . '</span>
-                <p>'. $p->getDesc() . '</p>
+                <p>' . $p->getDesc() . '</p>
             </div>
             <div class="card-action">
-            <a href="'.$url_product.'">' . $p->getPrice() . '€</a>
-</div>  
+            <a href="' . $url_product . '">' . $p->getPrice() . '€</a>
+            
+            <a href="' . $url_delete . '">Supprimer</a>
 </div>
-        </div>'."\n";
+</div>
+        </div>' . "\n";
     }
-?>
+    ?>
 </div>
