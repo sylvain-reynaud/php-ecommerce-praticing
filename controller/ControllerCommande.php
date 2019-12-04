3834edc9e5bb1b0ec8156d77f765b56b073a14c6
Sylvain Reynaud <sylvainreynaud@protonmail.com>
2019-12-04 12:01:06 +0100

    commenté

--- controller/ControllerCommande.php
+++ controller/ControllerCommande.php
@@ -5,12 +5,16 @@ require_once File::build_path(array("model", "ModelCommande.php"));
 
 class ControllerCommande
 {
+    /**
+     * Afficher la page de toutes les commandes d'un user
+     * @param $userId   id de l'utilisateur qui veut afficher ses commandes
+     */
     public static function readAllOfUser($userId)
     {
         $tab = ModelCommande::getAllOrdersOfUser($userId);  //appel au modèle pour gerer la BD
         $controller = 'commandes';
         $view = 'list';
         $pagetitle = 'Liste des commandes';
-        require(File::build_path(array("view", "view.php")));  //"redirige" vers la vue
+        require(File::build_path(array("view", "view.php")));
     }
 }
