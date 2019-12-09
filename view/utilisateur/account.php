4e720fb88a52f20e158d3a880fd144087284a30e
Sylvain Reynaud <sylvainreynaud@protonmail.com>
2019-12-09 15:08:23 +0100

    localhost fix

--- view/utilisateur/account.php
+++ view/utilisateur/account.php
@@ -1,4 +1,4 @@
 <ul>
     <li><a href="index.php?action=update&controller=ControllerUtilisateur">Modifier mon compte</a></li>
-    <li><a href="http://localhost/projet-php/index.php?action=readAllOfUser&controller=ControllerCommande">Voir mes commandes</a></li>
+    <li><a href="index.php?action=readAllOfUser&controller=ControllerCommande">Voir mes commandes</a></li>
 </ul>
